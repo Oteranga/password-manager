6aca4d654a1ae916f1932af7f95b5bef992c700f
Fgarcia19 <fabrizio.garcia@utec.edu.pe>
2022-06-09 01:43:13 -0500

    extract forms in backend

--- Proyecto/proyecto/keepass/views.py
+++ Proyecto/proyecto/keepass/views.py
@@ -1,11 +1,27 @@
+import email
 from django.shortcuts import render
-
+from keepass.forms import RegisterForm
 # Create your views here.
 
 def login_page(request):
     return render(request,'login.html')
 
 def register_page(request):
+    if request.method == "POST":
+      #Get the posted form
+      uname = request.POST['uname']
+      email = request.POST['email']
+      psw = request.POST['psw']
+      mpsw = request.POST['mpsw']
+      #verificar seguridad de contraseñas
+      #si todo bien
+        #hash contraseñas y guardar usuario retornar main_page
+      #sino return register marcando errores
+    else:
+        MyLoginForm = RegisterForm()
+    return render(request,'register.html')
+		
+    return render(request, 'loggedin.html', {"username" : username})
     return render(request,'register.html')
 
 def main_page(request):
