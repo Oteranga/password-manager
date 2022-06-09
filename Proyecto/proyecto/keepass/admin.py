6ab3876dd3897fc314104742d7bc0fc807c306fb
Oteranga <alejandro.otero@utec.edu.pe>
2022-06-09 01:07:19 -0500

    Models migrated

--- Proyecto/proyecto/keepass/admin.py
+++ Proyecto/proyecto/keepass/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User,Password
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Password)
--- Proyecto/proyecto/keepass/models.py
+++ Proyecto/proyecto/keepass/models.py
@@ -4,17 +4,26 @@ from django.db import models
 
 # Create your models here.
 
+#null = true para que no sea obligatorio el campo en la base de datos
+#blank = true para que no sea obligatorio rellenarlo en el formulario
+
 class User(models.Model):
     id = models.AutoField(primary_key= True)
-    username = models.TextField()
-    email= models.TextField()
-    password = models.TextField()
-    masterpassword = models.TextField()
+    username = models.TextField(max_length=100)
+    email= models.TextField(max_length=100)
+    user_password = models.TextField(max_length=100, null=False, default=str(username)) # password for authentication
+    masterpassword = models.TextField(max_length=300) # password for encryption
+
+    def __str__(self):
+        return self.username
 
 class Password(models.Model):
     id = models.AutoField(primary_key= True)
-    user_id = models.IntegerField()
-    username = models.TextField()
-    email = models.TextField()
-    destination_url = models.TextField()
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    username = models.TextField(max_length=100)
+    email = models.EmailField()
+    destination_url = models.URLField()
     password = models.TextField()
+
+    def __str__(self):
+        return self.destination_url
